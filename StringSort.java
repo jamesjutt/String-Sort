ed95f47c85a8857f4edd6be620fe1a8d73472003
James Jutt <44848902+jamesjutt@users.noreply.github.com>
2019-05-11 21:00:21 -0400

    Update StringSort.java

--- StringSort.java
+++ StringSort.java
@@ -1,7 +1,6 @@
 /**
  * StringSort.java
  * James Jutt
- * 2/13/19
  * Application that allows the user to enter up to 15 Strings and stores them in an array.
  * The user can quit early. The app will then display the strings entered in alphabetical order
  */
